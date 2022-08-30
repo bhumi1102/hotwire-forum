5b2f24186a1bb6faef6fc8d83719f669df1dd6ee
Bhumi Shah <bhumi1102@gmail.com>
2022-08-30 10:36:45 -0500

    setup navbar with real sign up, login, edit user links

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
+
+//todo added this to use method delete for users/sign_out, but does not work yet.
+//import Rails from "@rails/ujs"
+//Rails.start()
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
