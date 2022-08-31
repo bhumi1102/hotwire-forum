e6dad273550ae92ae7398b703e828873174d7572
Bhumi Shah <bhumi1102@gmail.com>
2022-08-31 09:23:08 -0500

    add discussion index controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: "main#index"
   get 'main/index'
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :discussions, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
