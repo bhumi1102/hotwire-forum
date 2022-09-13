62e51ec8cb8be9578ded2c06551e6782d17847fc
Bhumi Shah <bhumi1102@gmail.com>
2022-09-13 14:25:08 -0500

    create form submission rendering turbo response inside a turbo_test element on the current page

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -12,9 +12,14 @@ class DiscussionsController < ApplicationController
   def create
     @discussion = Discussion.new(discussion_params)
 
-    respond_to do |format| 
+    respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: "Discussion created successfully" }
+      format.html { redirect_to discussions_path, notice: "Discussion created", status: :see_other }
+        #format.turbo_stream { render turbo_stream: turbo_stream.append(@discussion, "<p>Hello from controller</p>") }
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append("turbo_test", partial: "discussions/discussion",
+          locals: { discussion: @discussion })
+      end
       else
         format.html { render :new, status: :unprocessable_entity }
       end
