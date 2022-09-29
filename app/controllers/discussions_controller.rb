870414f59a93685b6c44e88912c8037c7fdb5dc8
Bhumi Shah <bhumi1102@gmail.com>
2022-09-29 15:22:17 -0500

    add realtime broadcast for changes to discussion within category list

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -35,7 +35,24 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
+        
         @discussion.broadcast_replace(partial: "discussions/header", locals: { discussion: @discussion})
+        
+        if @discussion.saved_change_to_category_id?
+          old_category_id, new_category_id = @discussion.saved_change_to_category_id
+
+          old_category = Category.find(old_category_id)
+          new_category = Category.find(new_category_id)
+
+          # remove it from old category / insert to new category
+          @discussion.broadcast_remove_to(old_category)
+          @discussion.broadcast_prepend_to(new_category)
+
+          # reload so that the discussions_count gets updated on broadcasts
+          old_category.reload.broadcast_replace_to("categories")
+          new_category.reload.broadcast_replace_to("categories")
+        end
+
         format.html { redirect_to @discussion, notice: "Discussion updated", status: :see_other }
       else
         format.html { render :edit, status: :unprocessable_entity }
