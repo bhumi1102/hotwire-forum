d033bbaf229c036edba4bbfee763a19c28088f15
Bhumi Shah <bhumi1102@gmail.com>
2022-10-05 09:16:54 -0500

    add discussion_subscription model with inclusion validation

---
+++ app/models/discussion_subscription.rb
@@ -0,0 +1,18 @@
+class DiscussionSubscription < ApplicationRecord
+  belongs_to :discussion
+  belongs_to :user
+
+  scope :optin, -> { where(subscription_type: :optin) }
+  scope :optout, -> { where(subscription_type: :optout) }
+
+  validates :subscription_type, presence: true, inclusion: { in: ["optin", "optout"] }
+
+  def toggle!
+    case subscription_type
+    when "optin"
+      update(subscription_type: "optout")
+    when "optout"
+      update(subscription_type: "optin")
+    end
+  end
+end
